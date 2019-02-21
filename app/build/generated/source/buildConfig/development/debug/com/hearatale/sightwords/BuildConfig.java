6bade86c1d618b1ff6bca73f29d6f0e4fc7a5193
Yizra <yizra.g@gmail.com>
2019-02-21 17:26:09 -0500

    changed version code

--- app/build/generated/source/buildConfig/development/debug/com/hearatale/sightwords/BuildConfig.java
+++ app/build/generated/source/buildConfig/development/debug/com/hearatale/sightwords/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.hearatale.sightwords.dev";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "development";
-  public static final int VERSION_CODE = 123;
-  public static final String VERSION_NAME = "1.2.3";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
   // Fields from product flavor: development
   public static final String ISDEVELOPING = "working";
 }
--- app/build/generated/source/buildConfig/production/release/com/hearatale/sightwords/BuildConfig.java
+++ app/build/generated/source/buildConfig/production/release/com/hearatale/sightwords/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.hearatale.sightwords.production";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "production";
-  public static final int VERSION_CODE = 123;
-  public static final String VERSION_NAME = "1.2.3";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
   // Fields from product flavor: production
   public static final String ISDEVELOPING = "none";
 }
